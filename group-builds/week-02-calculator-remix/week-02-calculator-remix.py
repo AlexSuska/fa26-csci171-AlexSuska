79577677792ff19854481c436f5a20994f8398a5
AlexSuska <cora.suska@principia.edu>
2026-09-02 16:25:29 -0500

    Update week-02-calculator-remix.py

--- group-builds/week-02-calculator-remix/week-02-calculator-remix.py
+++ group-builds/week-02-calculator-remix/week-02-calculator-remix.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-"""Week 02 group build scratch file."""
-
-
-def main():
-    print("Week 02 group build: Calculator Remix")
-    
-    num1 = float( input("Please give me a number. ") )
-    num2 = float( input("One more. ") )
-    print("The sum is", num1 + num2)
-    print("The difference is", num1 - num2)
-    print("The product is", num1 * num2)
-    print("The quotient is", num1 / num2)
-
-
-
-if __name__ == "__main__":
-    main()
-=======
 """Week 02 group build scratch file."""
 
 
@@ -36,5 +17,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
->>>>>>> f681422 (Updated week-02-calculator-remix with new changes)
